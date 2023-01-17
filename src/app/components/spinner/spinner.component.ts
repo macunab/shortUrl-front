fbca0acdb4d9b2c5fa998bad51b2e14139e23752
Marshall <mn.acunab@gmail.com>
2023-01-17 18:11:47 -0300

    add spinner interceptor

--- src/app/components/spinner/spinner.component.ts
+++ src/app/components/spinner/spinner.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { delay } from 'rxjs';
+import { SpinnerService } from 'src/app/services/spinner.service';
 
 @Component({
   selector: 'app-spinner',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SpinnerComponent implements OnInit {
 
-  constructor() { }
+  isLoading: boolean = false;
+
+  constructor(private spinnerService: SpinnerService) { }
 
   ngOnInit(): void {
+    this.spinnerService.spinnerSub
+      .pipe(delay(0))
+        .subscribe((loading) => {
+          this.isLoading = loading;
+        })
   }
 
 }
--- src/app/interceptors/spinner.interceptor.ts
+++ src/app/interceptors/spinner.interceptor.ts
@@ -1,12 +1,19 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
-import { Observable } from "rxjs";
+import { delay, finalize, Observable } from "rxjs";
+import { SpinnerService } from "../services/spinner.service";
 
 
 export class SpinnerInterceptor implements HttpInterceptor {
+
+    constructor(private spinnerService: SpinnerService) {}
     
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        
-        return next.handle(req);
+        this.spinnerService.setLoading(true, req.url);
+        return next.handle(req)
+            .pipe(
+                delay(0),
+                finalize(() => this.spinnerService.setLoading(false, req.url))
+            );
     }
 
 }
